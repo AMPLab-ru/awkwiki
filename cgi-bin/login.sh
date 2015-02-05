16bcf5a13dc3a70ce9ee95ebb7e3e1209588519a
root <root@olly>
2015-02-05 04:03:25 +0300

    passwd file added

--- cgi-bin/login.sh
+++ cgi-bin/login.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+passwd_path="./awkpasswd"
+
 test -z "$1" -o -z "$2" && \
     { echo "Provide username and password"; exit 1; }
 
@@ -9,9 +11,6 @@ while read line; do
     username=${line%:*}
     password=${line#*:}
     test "$username" = "$1" -a "$password" = "$hash" && exit 0
-done << EOF
-admin:e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4
-user:b3daa77b4c04a9551b8781d03191fe098f325e67
-EOF
+done < "$passwd_path"
 
 exit 1
